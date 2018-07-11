9255a2d4515b1093063d143811bf2bb53ffbb0fe
iffig <isabella.figueroa@colorado.edu>
2018-07-10 18:52:59 -0600

    server.js fix pt. III

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
-// Help found at:
-// https://medium.com/@hellotunmbi/how-to-deploy-angular-application-to-heroku-1d56e09c5147
+//Install express server
 const express = require('express');
 const path = require('path');
 
 const app = express();
 
+// Serve only the static files form the dist directory
 app.use(express.static('./dist/ifigueroa'));
 
 app.get('/*', function(req,res) {
-  res.sendFile(path.join('./dist/ifigueroa/index.html'));
+
+res.sendFile(path.join(__dirname,'/dist/ifigueroa/index.html'));
 });
+
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
