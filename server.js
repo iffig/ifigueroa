88e44344e852dc052e4a2cc5b932315d4bc17bb2
iffig <isabella.figueroa@colorado.edu>
2018-07-10 18:41:25 -0600

    server.js fix

--- server.js
+++ server.js
@@ -5,11 +5,10 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/ifigueroa'));
 
 app.get('/*', function(req,res) {
-
-// res.sendFile(path.join(__dirname+'/dist/index.html'));
+  res.sendFile(path.join(__dirname+'/dist/ifigueroa/index.html'));
 });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
